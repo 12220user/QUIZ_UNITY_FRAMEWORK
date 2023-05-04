adf57f8f69c5023c2b82fea2583de2f8468d3e4e
12220 <supermansec@gmail.com>
2023-05-04 17:07:56 +0300

    завершена интеграция с unity, пакет полностью приспособлен к интеграции в виде package из git

--- Scripts/QUIZ_ENGINE.cs
+++ Scripts/QUIZ_ENGINE.cs
@@ -6,13 +6,32 @@ using UnityEngine;
 namespace WNF.Quiz{
     public class QUIZ_ENGINE : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        #region consants
+        private static readonly string PACKAGE_NAME = "Com.12220.quiz_framework";
+        private static readonly string FRAME_ASSET_PATH = "Packages/"+PACKAGE_NAME+"/Prefabs/QuizFrame.prefab";
+        private static readonly string FRAME_ASSET_PATH_DEV = "Assets/QUiZ_UNITY_FRAMEWORK/Prefabs/QuizFrame.prefab";
+        #endregion
+
+        #region Parametrs is visible
+
+        [Tooltip("јктивен если идет разработка плагина (не игры), а именно плагина, в конечном проекте всегда отключен")]
+        [SerializeField] private bool isDevelop = false;
+
+        #endregion
+
+        #region Parametrs unvisible
+
+
+
+        #endregion
+
+        private void Awake() => init();
+
+        private void init()
         {
-            var asset = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/com.12220.quiz_framework/Prefubs/QuizFrame.prefab");
+            // Load frame canvas with event system
+            var asset = AssetDatabase.LoadAssetAtPath<GameObject>(isDevelop?FRAME_ASSET_PATH_DEV:FRAME_ASSET_PATH);
             Instantiate(asset);
         }
-
-        
     }
 }
