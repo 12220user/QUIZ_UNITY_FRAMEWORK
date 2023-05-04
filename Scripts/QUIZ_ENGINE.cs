6e875407f38c74b4b2db67a33262d28e03d7f823
12220 <supermansec@gmail.com>
2023-05-04 16:24:54 +0300

    add assembly def

--- Scripts/QUIZ_ENGINE.cs
+++ Scripts/QUIZ_ENGINE.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace WNF.Quiz{
-    public class QuizEngine : MonoBehaviour
+    public class QUIZ_ENGINE : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
